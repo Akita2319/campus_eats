e656fc5373a633d2a49d6688e53fe80c05b10d0c
Akita2319 <2311140019p@ed.fuk.kindai.ac.jp>
2026-01-14 07:19:41 +0000

    Fix review content to comment

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :shop
 
-  validates :content, presence: true
+  validates :comment, presence: true
   validates :rating, inclusion: { in: 1..5 }
 end
